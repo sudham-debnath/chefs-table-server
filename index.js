bd1fca067389f3143a1e0552d650babe85b31969
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-04 02:39:14 +0600

    Updating Algorithm

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -12,6 +14,13 @@ const allData = require(".//data/data.json");
 app.get("/allData", (req, res) => {
     res.send(allData);
 });
+
+app.get("/allData/:id", (req, res) => {
+    const id = req.params.id;
+    const chef = allData[0].chefs.find((item) => item.id == id);
+    res.send(chef);
+  });
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
